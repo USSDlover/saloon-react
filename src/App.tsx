ebb04b80d9a1d25d4910448e43b0ebe477832ec6
Alireza Razinejad <a.razinejad@gmail.com>
2022-07-20 02:06:34 +0430

    The creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+        <h1>Saloon APP</h1>
+        <nav
+            style={{
+                borderBottom: "solid 1px",
+                paddingBottom: "1rem"
+            }}
+        >
+            <Link to="/invoices">Invoices</Link> | {" "}
+            <Link to="/expenses">Expenses</Link> | {" "}
+            <Link to="/appointment">Appointment</Link>
+        </nav>
+        <section>
+            <ul>
+                <li>
+                    <a
+                        href="https://reactrouter.com/docs/en/v6/getting-started/tutorial"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        React Router Docs
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://redux.js.org/introduction/getting-started/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Redux Docs
+                    </a>
+                </li>
+            </ul>
+        </section>
     </div>
   );
 }
